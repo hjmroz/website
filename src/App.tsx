e708466e75abeb934ec4a433cc6a13bd89db2332
Herb Mroz <87573533+hjmroz@users.noreply.github.com>
2024-09-24 13:00:40 -0400

    [chore] Tailwind Integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import '../src/styles/global.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <div className="flex flex-col">
+          <button>Button</button>
+          <h1 className='text-3xl font-bold underline'>Hello World!</h1>
+        </div>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
